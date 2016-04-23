0ba2b0bb3ab9e80cf3e6e1e14305c4f9ac68c335
sdlong <sdlong.jeng@gmail.com>
2016-04-23 15:29:12 +0800

    (chap11 F1-3) use cart patial on application.html

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
