6b47b178ee88bc98f73f8c6e5d1891c148f5e972
sdlong <sdlong.jeng@gmail.com>
2016-04-23 14:43:54 +0800

    (chap10 E3-2) refactor carts/show ( count & show total_price )

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add unique products" do
+    cart = Cart.create
+    book_one = products(:one)
+    book_two  = products(:two)
+    cart.add_product(book_one.id).save!
+    cart.add_product(book_two.id).save!
+    assert_equal 2, cart.line_items.size
+    assert_equal book_one.price + book_two.price, cart.total_price
+  end
+
+  test "add_duplicate_product" do
+    cart = Cart.create
+    ruby_book = products(:ruby)
+    cart.add_product(ruby_book.id).save!
+    cart.add_product(ruby_book.id).save!
+    assert_equal 2*ruby_book.price, cart.total_price
+    assert_equal 1, cart.line_items.size
+    assert_equal 2, cart.line_items[0].quantity
+  end
 end
