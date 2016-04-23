ec8615838083c4bfe0403a1128af8765eafab238
sdlong <sdlong.jeng@gmail.com>
2016-04-23 12:04:18 +0800

    (chap9 D1-1) Generate scaffold cart
    rails g scaffold cart
    
    rake db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :carts
   root 'store#index', as: 'store'
 
   get 'store/index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413104256) do
+ActiveRecord::Schema.define(version: 20160423040335) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
