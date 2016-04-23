2aa4246090d9356798127247cdbbf30df683ae12
sdlong <sdlong.jeng@gmail.com>
2016-04-23 14:03:23 +0800

    (chap10 E1-3) Migration: combine_items_in_cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423054546) do
+ActiveRecord::Schema.define(version: 20160423055837) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
