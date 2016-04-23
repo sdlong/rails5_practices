257375dcb60ec6278e01a0a3a49df569a3117d13
sdlong <sdlong.jeng@gmail.com>
2016-04-23 11:54:42 +0800

    (chap8 C5) Caching of Partial Results

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,8 @@ class Product < ApplicationRecord
     with:    %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
