b0d2f736c756db3bfaeb9b6d54d17a566977625b
sdlong <sdlong.jeng@gmail.com>
2016-04-26 10:00:46 +0800

    (chap13 H1-5) Add maiiler test

--- test/mailers/order_notifier_mailer_test.rb
+++ test/mailers/order_notifier_mailer_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class OrderNotifierMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifierMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifierMailer.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifierMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifierMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /<td>1&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/,
+      mail.body.encoded
   end
-
 end
