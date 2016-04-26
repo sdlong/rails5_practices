d3cd78dbea7ece361059308d222bca89948f5ef5
sdlong <sdlong.jeng@gmail.com>
2016-04-26 09:35:37 +0800

    (chap13 H1-1) Email Configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,21 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Don't actually send emails
+  config.action_mailer.delivery_method = :test
+  #
+  # Alternate configuration example, using gmail:
+  #   config.action_mailer.delivery_method = :smtp
+  #   config.action_mailer.smtp_settings = {
+  #     address:        "smtp.gmail.com",
+  #     port:           587,
+  #     domain:         "domain.of.sender.net",
+  #     authentication: "plain",
+  #     user_name:      "dave",
+  #     password:       "secret",
+  #     enable_starttls_auto: true
+  #   }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
