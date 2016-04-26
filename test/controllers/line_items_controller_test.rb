5d3de5001463a858f2f88612ebb387918fb727f9
sdlong <sdlong.jeng@gmail.com>
2016-04-26 08:14:46 +0800

    (chap11 F5-3) add ajax test

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -48,4 +48,16 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to line_items_path
   end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id },
+        xhr: true
+    end
+
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
+  end
 end
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -10,4 +10,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
+  test "markup needed for store.js.coffee is in place" do
+    get store_index_url
+    assert_select '.store .entry > img', 3
+    assert_select '.entry input[type=submit]', 3
+  end
 end
