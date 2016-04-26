476127b1bffd5933d99b7fa94c4ce47ed3b09c4d
sdlong <sdlong.jeng@gmail.com>
2016-04-26 08:47:23 +0800

    (chap12 G1-4) Add validate in model order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
