30bc1e2dd85f48464e790f8dc70f3fd3bf193bb4
sdlong <sdlong.jeng@gmail.com>
2016-04-26 10:16:04 +0800

    (chap14 I1-1) Generate scaffold user
    rails g scaffold User name:string password:digest
    
    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425234907) do
+ActiveRecord::Schema.define(version: 20160426021551) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 20160425234907) do
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
