c3af7248d90b939d40019bfb7dea31f9d8431a2b
sdlong <sdlong.jeng@gmail.com>
2016-04-26 12:35:12 +0800

    (chap15 J1) Selecting the Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_i18n_locale_from_params
   before_action :authorize
 
   protected
@@ -10,6 +11,18 @@ class ApplicationController < ActionController::Base
   def authorize
     unless User.find_by(id: session[:user_id])
       redirect_to login_url, alert: "Please log in"
+    end
+  end
+
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] =
+          "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,17 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store'
-
-  get 'store/index'
 
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    get 'store/index'
+    root 'store#index', as: 'store', via: :all
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
